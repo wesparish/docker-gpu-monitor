f33e5ce4c8c64f2177fa83499c9ab9e3a2862c3d
wes <wes@home.local>
2017-12-22 06:05:13 -0600

    Updates to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker build -t wesparish/gpu-monitor:nvidia . && \
-  docker tag wesparish/gpu-monitor:nvidia wesparish/gpu-monitor:nvidia && \
-  docker push wesparish/gpu-monitor:nvidia
+docker build -t wesparish/gpu-monitor . && \
+  docker push wesparish/gpu-monitor
